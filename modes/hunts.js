4d8a8f50d53b12e9ed858d74df4672629f3af138
dioh-cpu <dioh291@gmail.com>
2026-09-02 22:03:04 -0300

    V27: forçar bundle de balanceamento nas Hunts

--- modes/hunts.js
+++ modes/hunts.js
@@ -4,5 +4,5 @@ window.PSY.registerModePackage("hunts", {
   name:"HUNTS",
   desc:"Hunts por região, nível e tipo.",
   entry:["openHunts"],
-  deps:["bundles/v15-album-gym.js", "bundles/v19-consolidated.js?v=CAPTURE_ELEMENTAL_V22_20260902", "bundles/v20-systems.js?v=QUEST_UNICO_V20_20260902", "bundles/v47-survivor-hunts.js", "bundles/v50-compat.js"]
+  deps:["bundles/v15-album-gym.js", "bundles/v19-consolidated.js?v=CAPTURE_ELEMENTAL_V22_20260902", "bundles/v20-systems.js?v=QUEST_UNICO_V20_20260902", "bundles/v47-survivor-hunts.js", "bundles/v50-compat.js?v=TIER_RARITY_V27_20260903"]
 });
