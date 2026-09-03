02cd87f95558a7dcc2078590bf31e57f4f35562f
dioh-cpu <dioh291@gmail.com>
2026-09-02 22:40:26 -0300

    V28: unificar runtime Survivor nos Talentos

--- modes/trainer.js
+++ modes/trainer.js
@@ -4,5 +4,5 @@ window.PSY.registerModePackage("trainer", {
   name:"TALENTOS",
   desc:"Árvore e bônus permanentes do Treinador.",
   entry:["openTrainerShop"],
-  deps:["bundles/v15-album-gym.js", "bundles/v19-consolidated.js?v=CAPTURE_ELEMENTAL_V22_20260902", "bundles/v20-systems.js?v=QUEST_UNICO_V20_20260902", "bundles/v47-survivor-hunts.js", "bundles/v50-compat.js"]
+  deps:["bundles/v15-album-gym.js", "bundles/v19-consolidated.js?v=CAPTURE_ELEMENTAL_V22_20260902", "bundles/v20-systems.js?v=QUEST_UNICO_V20_20260902", "bundles/v47-survivor-hunts-v9.js?v=SURVIVOR_V28_20260903", "bundles/v50-compat.js?v=TIER_RARITY_V27_20260903", "core/survivor-psyduck-v28.js?build=SURVIVOR_V28_20260903"]
 });
