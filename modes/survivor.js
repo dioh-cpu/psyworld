60bf27a215eebfb65472e964dc33b5fcddbd3f5d
dioh-cpu <dioh291@gmail.com>
2026-09-02 22:41:10 -0300

    V28: alinhar manifesto alternativo do Survivor

--- modes/survivor.js
+++ modes/survivor.js
@@ -4,5 +4,5 @@ window.PSY.registerModePackage("survivor", {
   name:"PSYDUCK / SURVIVOR",
   desc:"Psyduck Supremo, Survivor, Provações e Ascensão.",
   entry:["openPsyduckDungeon5", "psy19OpenPsyduck"],
-  deps:["bundles/v15-album-gym.js", "bundles/v19-consolidated.js?v=CAPTURE_ELEMENTAL_V22_20260902", "bundles/v20-systems.js?v=QUEST_UNICO_V20_20260902", "bundles/v47-survivor-hunts-v9.js?v=QUEST_CAPTURE_FORTUNA_V19_20260902", "bundles/v50-compat.js", "bundles/psyduck-battle.js"]
+  deps:["bundles/v15-album-gym.js", "bundles/v19-consolidated.js?v=CAPTURE_ELEMENTAL_V22_20260902", "bundles/v20-systems.js?v=QUEST_UNICO_V20_20260902", "bundles/v47-survivor-hunts-v9.js?v=SURVIVOR_V28_20260903", "bundles/v50-compat.js?v=TIER_RARITY_V27_20260903", "bundles/psyduck-battle.js?build=SURVIVOR_V28_20260903", "core/survivor-psyduck-v28.js?build=SURVIVOR_V28_20260903"]
 });
